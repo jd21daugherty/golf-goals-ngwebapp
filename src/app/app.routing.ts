fce19fb58901d0df71e68231ebfa5ae440429f12
Unknown <costello.ritt@gmail.com>
2018-01-16 18:55:45 -0600

    add routing to nav bar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,10 @@
 import { Routes } from "@angular/router";
 
 import { StartRoundComponent } from "./pages/start-round/start-round.component";
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const appRoutes: Routes = [
-    {path: 'start-round', component: StartRoundComponent }
-]
+    {path: 'start-round', component: StartRoundComponent },
+    {path: 'home', component: HomePageComponent },
+    {path: '', redirectTo: 'home', pathMatch: 'full' }
+];
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SharedModule } from '../shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent
